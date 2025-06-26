2246c922d15921229015b847886492453fd8b2f2
Youssef Kashef <kashefy@ni.tu-berlin.de>
2025-06-26 12:02:49 +0200

    metho to load from dictionary

--- rsa/cache/rdm_cache.py
+++ rsa/cache/rdm_cache.py
@@ -8,31 +8,34 @@ class RDMCache:
         self.cache_dict = {}
         self.key_handler = CacheKey()
 
+    def load_from_dict(self, data):
+
+        version = data.get('version', None)
+        if version is None:
+            self.cache_dict = data
+
+        elif version == 250605:
+
+            if 'cache' in data and 'flist' in data:
+                flist = data['flist']
+                cache_dict_mapped = data['cache']
+                separator = data.get('separator', self.key_handler.separator)
+                # update separator
+                self.key_handler.separator = separator
+                for k, v in data['cache'].items():
+                    xi, yi = self.key_handler.split(k)
+                    x = flist[int(xi)]
+                    y = flist[int(yi)]
+                    k_new = self.key_handler.join(x, y)
+                    self.cache_dict[k_new] = v
+            else:
+                raise KeyError("Cache file does not contain 'cache' or 'flist' keys.")
+
     def load_from_file(self, fp_cache):
         with open(fp_cache, 'r') as h:
             data = yaml.safe_load(h)
 
-            version = data.get('version', None)
-
-            if version is None:
-                self.cache_dict = data
-
-            elif version == 250605:
-
-                if 'cache' in data and 'flist' in data:
-                    flist = data['flist']
-                    cache_dict_mapped = data['cache']
-                    separator = data.get('separator', self.key_handler.separator)
-                    # update separator
-                    self.key_handler.separator = separator
-                    for k, v in data['cache'].items():
-                        xi, yi = self.key_handler.split(k)
-                        x = flist[int(xi)]
-                        y = flist[int(yi)]
-                        k_new = self.key_handler.join(x, y)
-                        self.cache_dict[k_new] = v
-                else:
-                    raise KeyError("Cache file does not contain 'cache' or 'flist' keys.")
+            self.load_from_dict(data)
 
     def save_to_file(self, fp_dst):
         with open(fp_dst, 'w') as h:
